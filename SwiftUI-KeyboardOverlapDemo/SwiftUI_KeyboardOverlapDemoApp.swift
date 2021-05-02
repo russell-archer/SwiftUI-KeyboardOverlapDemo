c7929de1568eb2b259239f60234ea123a837d2b2
Russell Archer <russell.archer@mac.com>
2021-05-02 17:14:03 +0100

    Initial Commit

---
+++ SwiftUI-KeyboardOverlapDemo/SwiftUI_KeyboardOverlapDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_KeyboardOverlapDemoApp.swift
+//  SwiftUI-KeyboardOverlapDemo
+//
+//  Created by Russell Archer on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_KeyboardOverlapDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
